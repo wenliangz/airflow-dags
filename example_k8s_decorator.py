7b7c2d60cc00be6f6ae64a4ac18372f50d078053
Zhang <wenliang@bu.edu>
2023-02-22 13:17:37 -0500

    take away @dag

--- example_k8s_decorator.py
+++ example_k8s_decorator.py
@@ -27,18 +27,6 @@ def print_pattern():
         # ending line after each row
         print("\r")
 
-
-default_args = {
-    'owner': 'wenliang',
-    'retries': 5,
-    'retry_delay': timedelta(minutes=5)
-}
-
-@dag(dag_id='example_k8s_decorator',
-     default_args=default_args,
-     start_date=datetime(2023, 2, 20),
-     schedule_interval='@daily')
-
 execute_in_k8s_pod_instance = execute_in_k8s_pod()
 print_pattern_instance = print_pattern()
 execute_in_k8s_pod_instance >> print_pattern_instance
